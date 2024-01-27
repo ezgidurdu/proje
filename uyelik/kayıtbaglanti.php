d2e78353c7083a6d7ce1318bb6407d1f998aadf6
ezgidurdu <133711069+ezgidurdu@users.noreply.github.com>
2024-01-28 02:58:52 +0300

    Update kayıtbaglanti.php

--- uyelik/kayıtbaglanti.php
+++ uyelik/kayıtbaglanti.php
@@ -1,13 +1,1 @@
-<?php
-$host= "localhost";
-$kullanici= "root";
-$sifre= "";
-$vt= "kayıtlar";
 
-
-$kayıtbaglanti= mysqli_connect($host, $kullanici, $sifre, $vt);
-mysqli_set_charset($kayıtbaglanti,"UTF8");
-
-
-
-?>
